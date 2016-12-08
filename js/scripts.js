d17789bee9a2bfa3947fbf81c95eb63d4db70aad
Ivan Baresic <ivanbaresic@Ivans-MacBook-Pro.local>
2016-12-08 19:12:51 +0100

    css style blog

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,28 @@
 $(document).ready(function(){
 
 $('.blog__slider').slick({
-  dots: true,
+  dots: false,
   infinite: true,
   speed: 300,
   slidesToShow: 4,
   slidesToScroll: 1,
+  prevArrow:"<i class='fa fa-chevron-left slick-prev' aria-hidden='true'></i>",
+  nextArrow:"<i class='fa fa-chevron-right slick-next' aria-hidden='true'></i>",
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1280,
       settings: {
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         infinite: true,
-        dots: true
+        dots: false,
       }
     },
     {
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 1
       }
     },
     {
